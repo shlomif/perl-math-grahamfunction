b2f03ec6ea273c63616607258bbdc75619f1cf87
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2007-01-04 15:40:29 +0000

    Importing distro

---
+++ modules/Math-GrahamFunction/lib/Math/GrahamFunction.pm
@@ -0,0 +1,100 @@
+package Math::GrahamFunction;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Math::GrahamFunction - The great new Math::GrahamFunction!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Math::GrahamFunction;
+
+    my $foo = Math::GrahamFunction->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Shlomi Fish, C<< <shlomif at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-math-grahamfunction at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Math::GrahamFunction>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Math::GrahamFunction
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Math::GrahamFunction>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Math::GrahamFunction>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Math::GrahamFunction>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Math::GrahamFunction>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 Shlomi Fish, all rights reserved.
+
+This program is released under the following license: bsd
+
+=cut
+
+1; # End of Math::GrahamFunction
